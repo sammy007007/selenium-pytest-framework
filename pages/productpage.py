2aaeca19615fb3fafa721b4b9ad153bc1d7d9bde
sammy007007 <samarthkhare1996@gmail.com>
2026-04-07 17:46:34 +0530

    final

--- pages/productpage.py
+++ pages/productpage.py
@@ -3,19 +3,21 @@ from pages.basepage import BasePage
 
 
 class ProductPage(BasePage) :
-    URL ="https://sauce-demo.myshopify.com/products/striped-top?_pos=1&_sid=69495c96c&_ss=r"
-    add_to_cart_button = (By.XPATH,"//input[@id='add']")
-    my_cart_count = (By.XPATH,"//a[text()='My Cart ']/span/span")
+    URL ="https://adnabu-store-assignment1.myshopify.com/products/the-complete-snowboard?_pos=1&_sid=5e7408530&_ss=r"
+    add_to_cart_button = (By.XPATH,"//button[@name='add']")
+    my_cart_count = (By.XPATH,"//input[@class='quantity__input']")
+    your_cart_logo = (By.XPATH,"//h2[text()='Your cart']")
+
 
     def load(self):
         self.open_url(self.URL)
     
     def add_product_to_cart(self):
         self.click(self.add_to_cart_button)
-        self.wait_for_text(self.my_cart_count,"(1)")
+        self.wait_for_text(self.your_cart_logo,"Your cart")
     
 
     def get_cart_count(self):
-         return self.get_text(self.my_cart_count)
+         return self.get_text(self.my_cart_count).get_attribute("value")
 
     
