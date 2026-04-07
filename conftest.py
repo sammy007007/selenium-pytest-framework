afe6543f9a0d0cc9199667cd3a94b458e2195154
sammy007007 <samarthkhare1996@gmail.com>
2026-04-07 20:14:41 +0530

    refactored paramter logic

--- conftest.py
+++ conftest.py
@@ -4,14 +4,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from driverFactory import get_driver
+def pytest_addoption(parser):
+    parser.addoption("--store-pass", action="store")
 
 @pytest.fixture(scope="function")
-def driver():
+def driver(request):
+    store_password = request.config.getoption("--store-pass")
     driver=get_driver()
     wait = WebDriverWait(driver, 10) 
     driver.get("https://adnabu-store-assignment1.myshopify.com/password")
     driver.set_page_load_timeout(5)
-    driver.find_element(By.ID,"password").send_keys("AdNabuQA"+ Keys.ENTER)
+    driver.find_element(By.ID,"password").send_keys(store_password+ Keys.ENTER)
     wait.until(EC.invisibility_of_element_located((By.ID, "password"))) 
 
     yield driver
